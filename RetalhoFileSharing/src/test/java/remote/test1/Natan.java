0e058a372ef5a3383b0bbd310ba535c449d29fa7
natanmorais <morais_natan@hotmail.com>
2017-05-22 18:13:45 -0300

    Criado modelo do teste 1
    Modelo do teste 1. Teste para testar transferencia de arquivos entre dois computadores

---
+++ RetalhoFileSharing/src/test/java/remote/test1/Natan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.java.remote.test1;
+
+/**
+ *
+ * @author fir3destr0yer
+ */
+public class Natan {
+    //CRIA NÓ NORMAL
+    //BUSCA REDES DISPONÍVEIS
+    //ENTRA NA REDE DO TIAGO
+    //DISPONIBILIZA ARQUIVO PARA COMPARTILHAMENTO
+    //BUSCA ARQUIVOS DISPONÍVEIS
+    //BAIXA ARQUIVO DE TIAGO
+}
---
+++ RetalhoFileSharing/src/test/java/remote/test1/Tiago.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.java.remote.test1;
+
+/**
+ *
+ * @author fir3destr0yer
+ */
+public class Tiago {
+    //CRIA MASTER
+    //DISPONILIZA ARQUIVO PARA COMPARTILHAMENTO
+    //SLEEP 8 MIN
+    //BUSCA ARQUIVOS DISPONÍVEIS
+    //BAIXA ARQUIVO DE NATAN
+}
